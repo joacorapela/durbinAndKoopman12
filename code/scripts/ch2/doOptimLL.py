71c23ac21bc5362e7730fd40f5f718a8487b059b
joacorapela <joacorapela@gmail.com>
2023-11-01 01:47:32 +0000

    Minor changes (e.g., removed gradients)

--- code/scripts/ch2/doOptimLL.py
+++ code/scripts/ch2/doOptimLL.py
@@ -12,36 +12,36 @@ import tsa
 def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("--sim_res_number", type=int,
-                        help="simulation result number", default=372651)
+                        help="simulation result number", default=16347168)
     parser.add_argument("--a10", type=float,
                         help="initial condition for initial state mean",
-                        default=1.0)
+                        default=0.0)
     parser.add_argument("--P10", type=float,
                         help="initial condition for initial state variance",
-                        default=1e1)
+                        default=0.01)
     parser.add_argument("--sigma_epsilon0", type=float,
                         help=("initial condition for observations noise "
                               "variance"),
-                        default=0.5)
+                        default=1.0)
     parser.add_argument("--sigma_eta0", type=float,
                         help="initial condition for state noise variance",
-                        default=2.0)
+                        default=1.5)
     parser.add_argument("--sim_res_filename_pattern", type=str,
                         help="simulation result filename pattern",
-                        default="../../../data/{:08d}_simRes.pickle")
+                        default="../../../results/{:08d}_simRes.pickle")
     parser.add_argument("--est_res_metadata_filename_pattern",
                         help="estimation metadata filename pattern", type=str,
-                        default="../../../data/{:08d}_est_metadata.ini")
+                        default="../../../results/{:08d}_est_metadata.ini")
     parser.add_argument("--est_res_filename_pattern", type=str,
                         help="estimation result filename pattern",
-                        default="../../../data/{:08d}_est_res.pickle")
+                        default="../../../results/{:08d}_est_res.pickle")
 
     args = parser.parse_args()
     sim_res_number = args.sim_res_number
     a10 = args.a10
-    P10 = args.P10
-    sigma_epsilon0 = args.sigma_epsilon0
-    sigma_eta0 = args.sigma_eta0
+    lP10 = np.log(args.P10)
+    ls2ep0 = np.log(args.sigma_epsilon0**2)
+    ls2et0 = np.log(args.sigma_eta0**2)
     sim_res_filename_pattern = args.sim_res_filename_pattern
     est_res_metadata_filename_pattern = args.est_res_metadata_filename_pattern
     est_res_filename_pattern = args.est_res_filename_pattern
@@ -55,10 +55,12 @@ def main(argv):
         sim_res = pickle.load(f)
     y = sim_res["y"]
 
-    llmLLcalc = tsa.LocalLevelModelLogLikeCalculator(y=y)
-    params0 = np.array([a10, np.log(P10), np.log(sigma_epsilon0**2),
-                        np.log(sigma_eta0**2)])
-    options = {"maxiter": maxiter, "disp": disp}
+    params_to_estimate = ["ls2ep", "ls2et"]
+    params0 = [ls2ep0, ls2et0]
+    llmLLcalc = tsa.LocalLevelModelLogLikeCalculator(
+        y=y, params_to_estimate=params_to_estimate,
+        fixed_params_values={"a1": a10, "lP1": lP10})
+    options = {"gtol": 1e-10, "maxiter": maxiter, "disp": disp}
 
     def callback(intermediate_result: scipy.optimize.OptimizeResult):
         # print("LL(a1={:.02f}, lP1={:.02f}, ls2ep={:.02f}, ls2eta={:.02f})={:.02f}".format(
@@ -67,11 +69,13 @@ def main(argv):
         #     intermediate_result["x"][2],
         #     intermediate_result["x"][3],
         #     intermediate_result["fun"]))
-        print("LL(a1={:.02f}, lP1={:.02f}, ls2ep={:.02f}, ls2et={:.02f})".format(
+        # print("LL(ls2ep={:.02f}, ls2et={:.02f})".format(
+        #     intermediate_result[0],
+        #     intermediate_result[1]))
+        print("LL(ls2ep={:.02f}, ls2et={:.02f})".format(
             intermediate_result[0],
             intermediate_result[1],
-            intermediate_result[2],
-            intermediate_result[3]))
+        ))
 
     def fun(x):
         return -llmLLcalc.ll(x)
@@ -80,8 +84,10 @@ def main(argv):
         return -llmLLcalc.grad(x)
 
     optim_res = scipy.optimize.minimize(fun=fun, x0=params0,
-                                        method=method, jac=grad,
+                                        # method=method, jac=grad,
+                                        method="BFGS",
                                         options=options, callback=callback)
+    print(optim_res)
 
     breakpoint()
 
